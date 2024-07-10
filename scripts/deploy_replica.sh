14d4e8910eda3b27b586aed05a521e689390b172
detextre4 <detextre4@email.com>
2024-07-10 19:18:29 -0400

    config/change-branch

--- scripts/deploy_replica.sh
+++ scripts/deploy_replica.sh
@@ -5,7 +5,8 @@ firstArg=$1
 secondArg=$2
 
 # Change identity to `default`
-echo "====-Change identity to default-===="
+echo "====-Change identity to default and branch to develop-===="
+git checkout develop
 dfx identity use default
 
 # Deploy nns canisters
