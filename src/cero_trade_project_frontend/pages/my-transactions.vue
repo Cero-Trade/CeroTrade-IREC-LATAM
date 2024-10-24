6f2456e09ea09f23d3058a6b430ad07a4d6aca1a
detextre4 <detextre4@email.com>
2024-10-24 19:13:48 -0400

    fix/transaction-id+token-transaction-index

--- src/cero_trade_project_frontend/pages/my-transactions.vue
+++ src/cero_trade_project_frontend/pages/my-transactions.vue
@@ -129,6 +129,15 @@
         <span style="text-wrap: nowrap">{{ item.via }}</span>
       </template>
 
+      <template #[`item.token_tx_index`]="{ item }">
+        <span v-if="!item.token_tx_index">---</span>
+
+        <span v-else class="pointer acenter" :title="item.token_tx_index" @click="item.token_tx_index.copyToClipboard('Token Tx Block copied')">
+          {{ shortString(item.token_tx_index, {}) }}
+          <img src="@/assets/sources/icons/copy.svg" alt="copy icon" class="ml-2" style="width: 18px">
+        </span>
+      </template>
+
       <template #[`item.ledger_tx_hash`]="{ item }">
         <span v-if="!item.ledger_tx_hash">---</span>
 
@@ -435,6 +444,7 @@ energies = {
   { title: 'MWh', key: 'mwh', align: 'center', sortable: false },
   { title: 'Date', key: 'date', align: 'center', sortable: false },
   { title: 'Via', key: 'via', align: 'center', sortable: false },
+  { title: 'Token Tx Block', key: 'token_tx_index', align: 'center', sortable: false, width: "110px" },
   { title: 'Ledger Tx Block', key: 'ledger_tx_hash', align: 'center', sortable: false, width: "110px" },
   { title: 'Comission Tx Block', key: 'comission_tx_hash', align: 'center', sortable: false, width: "110px" },
 ],
@@ -536,6 +546,7 @@ async function getData() {
         date: item.date.toDateString(),
         price: item.priceE8S,
         via: item.method,
+        token_tx_index: item.tokenTxIndex,
         comission_tx_hash: item.comissionTxHash,
         ledger_tx_hash: item.ledgerTxHash,
       })
